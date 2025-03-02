20d3e7b7e1368aafe44fe47f0818bfa5819e3950
Amin.vz <amin.valizadeh.116.official@gmail.com>
2025-03-02 13:26:46 +0330

    fix: fix the limmit less loop

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [data, setData] = useState([]);
   const [lat, setLat] = useState(35.69439);
   const [lon, setLon] = useState(51.42151);
+  const [city, setCity] = useState('tehran')
 
-  fetch(
+  useEffect(() => {
+    fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=1b8d635a50039f3f09175d1be5555943`
   ).then((response) => response.json().then((result) => setData(result)));
 
+  }, [])
+  
   console.log(data)
 
   return (
     <>
-      
+
       <h2>hello</h2>
     </>
   );
